bfaa603207e151748029936cad523af491379c56
Divyansh Kulshreshtha <k.divyansh268@gmail.com>
2021-06-15 22:56:48 +0530

    Increasing functionality: input fields + updation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,8 +27,16 @@ class MyHomePage extends StatelessWidget {
       title: "Burger",
       amount: 120,
       date: DateTime.now(),
-    )
+    ),
+    Transaction(
+      id: "t3",
+      title: "Thaali",
+      amount: 140,
+      date: DateTime.now(),
+    ),
   ];
+  String titleInput;
+  String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +45,8 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
               width: double.infinity,
@@ -44,6 +54,39 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text("Chart!"),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                      onChanged: (value) {
+                        titleInput = value;
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: "Amount"),
+                      onChanged: (value) => amountInput = value,
+                    ),
+                    ElevatedButton(
+                      child: Text('Add Transaction'),
+                      onPressed: () {
+                        print(titleInput);
+                        print(amountInput);
+                      },
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.purple),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white)),
+                    ),
+                  ],
+                ),
               ),
             ),
             Column(
@@ -55,20 +98,33 @@ class MyHomePage extends StatelessWidget {
                       margin:
                           EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 2)),
+                          border: Border.all(color: Colors.purple, width: 2)),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        "\$${tx.amount}",
                         style: TextStyle(
-                          fontFamily: FontWeight.bold,
+                          fontWeight: FontWeight.bold,
                           fontSize: 20,
+                          color: Colors.purple,
                         ),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tx.title),
-                        Text(tx.date.toString()),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          DateFormat.yMMMEd().format(tx.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
                       ],
                     )
                   ],
