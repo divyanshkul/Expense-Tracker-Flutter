9c7cb0c89cccb15d32a692445103ba297e028c1b
Divyansh Kulshreshtha <k.divyansh268@gmail.com>
2021-06-17 23:49:00 +0530

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
-                child: Text("Chart!"),
+                child: Text("Chart!!"),
                 elevation: 5,
               ),
             ),
